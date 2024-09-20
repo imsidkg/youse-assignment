a8d805e779569fd0bf14867100a0c60a70757e62
imsidkg <siddharth8271skg2@gmail.com>
2024-09-20 15:33:11 +0530

    mongo con

---
+++ src/backend/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () : Promise<void> => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string)
+        console.log("Your mongoDB connectiooon was estab.");
+    } catch (error) {
+        console.log("Error connecting to mongoDB:", error );
+        process.exit(1);
+    }
+}
---
+++ src/backend/interface/ITask.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export interface ITask extends Document {
+  title: string;
+  description?: string;
+  status: 'To Do' | 'In Progress' | 'Completed';
+  priority: 'Low' | 'Medium' | 'High';
+  dueDate?: Date;
+  userId: string;
+}
